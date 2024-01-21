79f04644c2c6aea22d84bdb39a4eced0cdd86d93
Gustavi1729 <93688486+Gustavi1729@users.noreply.github.com>
2024-01-21 17:29:09 -0300

    Pastas Publicas
    Criação do servidor node básico, linkando com a pasta publica da árvore, e criação de arquivos básicos para testar se está tudo OK

---
+++ src/server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('../../public'));
+
+app.get('/', (req, res) => {
+  res.send('Main page!');
+});
+
+app.listen(3000, function () {
+  console.log('Server listening on port 3000');
+});
