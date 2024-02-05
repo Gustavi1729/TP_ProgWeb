3cad4dcca7c610a3a71d20c1b3da3af6e22d367d
Gustavi1729 <93688486+Gustavi1729@users.noreply.github.com>
2024-02-05 11:50:45 -0300

    Refinamento CSS e Javascript + animações

--- public/script.js
+++ public/script.js
@@ -1,33 +1,30 @@
-console.log("Oi tudo bom?")
-
 document.querySelector(".side-panel-toggle").addEventListener("click", () => {
-    document.querySelector(".wrapper").classList.toggle("side-panel-open");
-  });
-  
+  document.querySelector(".wrapper").classList.toggle("side-panel-open");
+});
+
 const perguntas = document.querySelectorAll(".pergunta");
 const chatbox = document.querySelector(".chatbox");
 const scrollAPI = document.querySelector("#API");
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 let userMessage = null; // Variable to store user's message
-const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
-    // Create a chat <li> element with passed message and className
-    const chatLi = document.createElement("li");
-    chatLi.classList.add("chat", `${className}`);
-    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
-    chatLi.innerHTML = chatContent;
-    chatLi.querySelector("p").textContent = message;
-    return chatLi; // return chat <li> element
+
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", `${className}`);
+  let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+  chatLi.innerHTML = chatContent;
+  chatLi.querySelector("p").textContent = message;
+  return chatLi;
 }
 const generateResponse = (chatElement) => {
-    const messageElement = chatElement.querySelector("p");
+  const messageElement = chatElement.querySelector("p");
 
-    message = {
-        mensagem: userMessage
-      };
-    const fetchOptions = {
+  message = {
+    mensagem: userMessage
+  };
+  const fetchOptions = {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
@@ -35,30 +32,27 @@ const generateResponse = (chatElement) => {
     },
     body: JSON.stringify(message)
   };
-    // Send POST request to API, get response and set the reponse as paragraph text
-    fetch('/', fetchOptions).then(res => res.text()).then(data => {
-        messageElement.textContent = data;
-    }).catch(() => {
-        messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Something went wrong. Please try again.";
-    }).finally(() => scrollAPI.scrollTo(0, chatbox.scrollHeight));
+  // Send POST request to API, get response and set the reponse as paragraph text
+  fetch('/', fetchOptions).then(res => res.text()).then(data => {
+    messageElement.textContent = data;
+  }).catch(() => {
+    messageElement.classList.add("error");
+    messageElement.textContent = "Algo deu errado! :(";
+  }).finally(() => scrollAPI.scrollTo(0, chatbox.scrollHeight));
 }
 const handleChat = () => {
-    userMessage = chatInput.value.trim();
-    if(!userMessage) return;
-    // Clear the input textarea and set its height to default
-    chatInput.value = "";
-    // Append the user's message to the chatbox
-    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+  userMessage = chatInput.value.trim();
+  if (!userMessage) return;
+  chatInput.value = "";
+  chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+  scrollAPI.scrollTo(0, chatbox.scrollHeight);
+
+  setTimeout(() => {
+    const incomingChatLi = createChatLi("...", "incoming");
+    chatbox.appendChild(incomingChatLi);
     scrollAPI.scrollTo(0, chatbox.scrollHeight);
-    
-    setTimeout(() => {
-        // Display "Thinking..." message while waiting for the response
-        const incomingChatLi = createChatLi("Thinking...", "incoming");
-        chatbox.appendChild(incomingChatLi);
-        scrollAPI.scrollTo(0, chatbox.scrollHeight);
-        generateResponse(incomingChatLi);
-    }, 600);
+    generateResponse(incomingChatLi);
+  }, 600);
 }
 
 sendChatBtn.addEventListener("click", handleChat);
@@ -66,10 +60,9 @@ sendChatBtn.addEventListener("click", handleChat);
 
 
 perguntas.forEach(item => {
-    item.addEventListener('click', () => {
-      console.log(`Clicked on: ${item.textContent}`);
-      chatInput.value = item.textContent;
-      handleChat()
-      // Add your event handling logic here
-    });
+  item.addEventListener('click', () => {
+    chatInput.value = item.textContent;
+    handleChat()
+
   });
+});
